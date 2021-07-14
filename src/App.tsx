2e50a0d790c71e12491a8c784d8197c195ce5502
ruhaha <videogame531@live.com>
2021-07-14 08:13:04 -0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+const rust = import('myWasm');
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    rust.then((m) => m.greet()).catch(console.error);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +16,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ wasm/README.md
@@ -0,0 +1,5 @@
+This is a placeholder workspace for wasm apps to go.
+
+In this directory run `cargo new LIB_NAME -lib` to create a library and start working
+
+Some code taken from https://github.com/rustwasm/wasm-pack-template
